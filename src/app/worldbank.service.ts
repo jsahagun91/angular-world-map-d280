afa00bb5f85abbf60d0a600aeb9af1e2d2d3eda4
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-07-20 13:55:44 -0700

    worldbank service

--- src/app/worldbank.service.ts
+++ src/app/worldbank.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,26 @@ export class WorldbankService {
   getCountryInfo(countryName: string) {
     return this.http.get(
       'https://api.worldbank.org/v2/country/${countryName}?format=json'
-    )''
+    );
+  }
+
+  setCountryInfo(countryName: string, countryData: any) {
+    return this.getCountryInfo(countryName).subscribe(
+      (response: any) => {
+        if (
+          Array.isArray(response) &&
+          response.length > 1 &&
+          Array.isArray(response[1]) &&
+          response[1].length > 0 
+        ) {
+          countryData = response[1][0]; // Assign fetched data to the local variable
+        } else {
+          console.warn('Unexpected API response structure:', response);
+        }
+      },
+      (error) => {
+        console.error('API Error:', error);
+      }
+    );
   }
 }
-
